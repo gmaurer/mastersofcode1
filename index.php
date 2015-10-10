72f893f157a1b11bf8ccee040be1af8b4626f4da
gmaurer <gmaurer007@gmail.com>
2015-10-10 16:36:23 -0500

    edited index for button somewhat

---
+++ index.php
@@ -0,0 +1,46 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+    <script src="http://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+
+
+
+</head>
+<header>
+
+
+
+</header>
+
+<style type="text/css">
+
+    .btn{
+
+        float: right;
+        
+    }
+
+
+</style>
+
+<body>
+    <div>
+        <a href="#" class="btn btn-info" role="button" id = 'btn1'>New User</a>
+        <a href="#" id="btnr" class="btn btn-info" role="button" >Existing User</a>
+            
+    </div>
+
+
+
+</body>
+
+<footer>
+
+
+
+
+</footer>
+</html>
