3a9b295012aa4df3c830bdaf3b3c22e13d46f7d5
jacobwbaird <jacobwbaird@gmail.com>
2015-10-10 16:39:18 -0500

    create new user file added

---
+++ createnew.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+
+<html>
+    <head>
+        <meta charset="UTF-8">
+        <title>Create New User</title>
+    </head>
+    <body>
+	<form>
+	    <input type="radio" name="membertype" value="organizer"> 
+	</form>
+    </body>
+</html>
